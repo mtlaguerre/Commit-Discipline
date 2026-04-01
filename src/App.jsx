35b8a8c50cbd4dce85fc867c998890ddc53cedc4
iMarcraft <marcusponders@prton.me>
2026-04-01 14:56:59 -0500

    Added Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
+import Footer from './Components/Footer.jsx';
 import './Components/Header.jsx'
 import Header from './Components/Header.jsx';
 
 function App() {
 	const headerContent = ["The Beginning", "This paragraph is meant for demonstration purposes. Nothing more, nothing less. Let this be ther start of your learning journey, at least learning from me..."];
+	const footerContent = "Made by Marcus Laguerre © copyrights protected."
 
 	return (
 		<>
 			<Header content={headerContent}/>
+			<Footer content={footerContent}/>
 		</>
 	)
 }
